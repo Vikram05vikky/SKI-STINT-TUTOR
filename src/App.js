9d3a385d0e5c69075f140da8085f85ffae996d9f
Vikram05vikky <vikram05sar@gmail.com>
2024-06-25 11:32:20 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import { SidebarProvider, Sidebar } from './components/Sidebar';
-import LandingPage from './components/LandingPage';
-import Task from './components/Tasks';
-import Users from './components/Users';
-import UserSB from './components/Users';
-import Userid from './components/Userid';
-import Navbar from './components/Navbar';
-// import WebLayout from './components/PrinciComp'; 
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { SidebarProvider, Sidebar } from "./components/Sidebar";
+import LandingPage from "./components/LandingPage";
+import Task from "./components/Tasks";
+import Users from "./components/Users";
+import UserSB from "./components/Users";
+// import Userid from './components/Userid';
+import Navbar from "./components/Navbar";
+// import WebLayout from './components/PrinciComp';
 
 export default function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -19,41 +19,35 @@ export default function App() {
 
   return (
     <Router>
-    {/* <WebLayout> */}
+      {/* <WebLayout> */}
       <SidebarProvider>
-        <div className={`app-wrapper ${sidebarOpen ? 'sidebar-open' : ''}`}>
+        <div className={`app-wrapper ${sidebarOpen ? "sidebar-open" : ""}`}>
           <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
           <div className="main-content">
             <Navbar toggleSidebar={toggleSidebar} />
             <div className="content">
               <Routes>
-                <Route path='/' element={<LandingPage />} />
-                <Route path='/landingpage' element={<LandingPage />} />
-                <Route path='/alltasks' element={<Task />} />
-                <Route path='/pendingtasks' element={<Task filterType="progress" />} />
-                <Route path='/completedtasks' element={<Task filterType="completed" />} />
-                <Route path='/yettasks' element={<Task filterType="yet" />} />
-                <Route path='/users' element={<UserSB />} />
-                <Route path='/user/:userName' element={<Userid />} />
-                <Route path='/allusers' element={<Users />} />
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/landingpage" element={<LandingPage />} />
+                <Route path="/alltasks" element={<Task />} />
+                <Route
+                  path="/pendingtasks"
+                  element={<Task filterType="progress" />}
+                />
+                <Route
+                  path="/completedtasks"
+                  element={<Task filterType="completed" />}
+                />
+                <Route path="/yettasks" element={<Task filterType="yet" />} />
+                <Route path="/users" element={<UserSB />} />
+                {/* <Route path="/user/:userName" element={<Userid />} /> */}
+                <Route path="/allusers" element={<Users />} />
               </Routes>
             </div>
           </div>
         </div>
       </SidebarProvider>
-       {/* </WebLayout> */}
+      {/* </WebLayout> */}
     </Router>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
